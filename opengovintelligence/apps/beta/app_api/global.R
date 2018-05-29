7144d2ca390e331290d18ceaf44de17c0dc6c451
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-05-29 16:47:17 +0100

    Created version of work<ness app
    - added API call for nomis claimant count data
    - added food banks and probation offices
    - improved ggplot2 chart appearance

---
+++ opengovintelligence/apps/beta/app_api/global.R
@@ -0,0 +1,65 @@
+## Work<ness app - using regularly updated nomis APIs ##
+
+# load necessary packages ---------------------------
+library(shiny)
+library(tidyverse)
+library(sf)
+library(spdep) 
+library(rgeos)
+library(leaflet)
+library(ggplot2) 
+library(DT)
+
+# load Trafford Data Lab's ggplot2 lab_theme() ---------------------------
+source("https://github.com/traffordDataLab/assets/raw/master/theme/ggplot2/theme_lab.R")
+
+# load data ---------------------------
+# Latest claimant count data from nomis
+source("data/nomis_api.R")
+
+# 2011 Census data stored as a flat file
+df <- read_csv("data/worklessness_data.csv") %>%
+  bind_rows(ucjsa) %>% 
+  mutate(lsoa11cd = factor(lsoa11cd),
+         lsoa11nm = factor(lsoa11nm),
+         lad17nm = factor(lad17nm),
+         measure = factor(measure),
+         value = as.integer(value))
+
+# source("data/sparql.R") # commented out because takes 45 seconds approx
+# IMD 2015 stored as a flat file but generated using SPARQL query
+imd <- read_csv("https://github.com/traffordDataLab/open_data/raw/master/imd_2015/IMD_2015_wide.csv",
+                col_types = cols(lsoa11cd = col_factor(NULL),
+                                 index_domain = col_factor(NULL),
+                                 decile = col_factor(1:10),
+                                 rank = col_integer(),
+                                 score = col_double()))
+
+# Lower-layer Super Output areas in Greater Manchester
+GM_lsoa <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/lookups/lsoa_to_ward_best-fit_lookup.geojson") %>% 
+  as('Spatial')
+
+# Local Authorities in Greater Manchester
+la <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/local_authority/2016/gm_local_authority_generalised.geojson") %>% 
+  rename(lad17cd = area_code, lad17nm = area_name) %>% 
+  mutate(centroid_lng = map_dbl(geometry, ~st_centroid(.x)[[1]]),
+         centroid_lat = map_dbl(geometry, ~st_centroid(.x)[[2]]))
+
+# Jobcentre Plus locations in Greater Manchester
+jcplus <- st_read("https://github.com/traffordDataLab/open_data/raw/master/job_centre_plus/jobcentreplus_gm.geojson") %>% 
+  rename(lad17cd = area_code, lad17nm = area_name)
+
+# Betting shops in Greater Manchester
+betting <- st_read("https://github.com/traffordDataLab/open_data/raw/master/betting_shops/bettingshops_gm.geojson") %>% 
+  rename(lad17cd = area_code, lad17nm = area_name)
+
+# General Practices in Greater Manchester
+gp <- st_read("https://github.com/traffordDataLab/open_data/raw/master/general_practice/GM_general_practices.geojson") %>% 
+  rename(lad17cd = area_code, lad17nm = area_name)
+
+# Food banks in Greater Manchester
+food_bank <- st_read("https://github.com/traffordDataLab/open_data/raw/master/food_banks/GM_food_banks.geojson")
+
+# Probation offices in Greater Manchester
+probation <- st_read("https://github.com/traffordDataLab/open_data/raw/master/probation/GM_probation_offices.geojson") %>% 
+  rename(lad17cd = area_code, lad17nm = area_name)
---
+++ opengovintelligence/apps/beta/app_api/ui.R
@@ -0,0 +1,48 @@
+## Work<ness app ##
+
+# User interface ---------------------------
+
+ui <- navbarPage(
+  title = div(img(src = "https://trafforddatalab.github.io/assets/logo/trafforddatalab_logo.svg", height="25", width="99"), 
+              "Work<ness app"), windowTitle = "Work<ness app",
+  tabPanel(title = "Maps",
+           div(class="shinyContainer",
+               tags$head(includeCSS("styles_base.css"), includeCSS("styles_shiny.css"), includeCSS("styles_map.css"),
+                         tags$style(HTML("table.imd td:nth-child(2), table.imd td:nth-child(3) { text-align: right; }"))),
+               leafletOutput("map", width = "100%", height = "100%"),
+               absolutePanel(id = "shinyControls", class = "panel panel-default controls", fixed = TRUE, draggable = TRUE,
+                             h4("Choose a local authority"),
+                             selectInput(inputId = "la",
+                                         label = NULL,
+                                         choices = c("Greater Manchester", levels(df$lad17nm)),
+                                         selected = "Greater Manchester"),
+                             h4("and a measure"),
+                             radioButtons(inputId = "measure",
+                                          label = NULL,
+                                          choices = c("Residents claiming JSA or Universal Credit",
+                                                      "Working-age adults with no qualifications",
+                                                      "Households with lone parent not in employment",
+                                                      "Social rented households"),
+                                          selected = "Residents claiming JSA or Universal Credit"),
+                             hr(),
+                             uiOutput("info")))),
+  tabPanel(title = "Charts",
+           fluidPage(
+             sidebarLayout(
+               sidebarPanel(width = 3,
+                            uiOutput("choose_area"),
+                            checkboxInput("multiple", label = "or multiple areas", value = FALSE),
+                            conditionalPanel(
+                              condition="input.multiple==true",
+                              h6("Output options"),
+                              checkboxInput("facet", label = "Facet by area", value = FALSE))),
+               mainPanel(plotOutput("ggplot_plot", width = "100%", height = "300px"),
+                         br(),
+                         DT::dataTableOutput("area_table"),
+                         br(),
+                         downloadButton("downloadData", "Download data"))))),
+  tabPanel(title = "About", 
+           fluidRow(
+             column(8, offset = 1,
+                    includeMarkdown("about.md"), style = "color:#212121")))
+)

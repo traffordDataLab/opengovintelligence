bdbabd6fa5b3400808e25315bd0f56002f7f6c17
Henry Partridge <henry.partridge@trafford.gov.uk>
2018-08-24 16:27:04 +0100

    Update pre-processing.R

--- opengovintelligence/gmdatastore/RDF/datasets/claimant-count/pre-processing.R
+++ opengovintelligence/gmdatastore/RDF/datasets/claimant-count/pre-processing.R
@@ -1,5 +1,10 @@
 library(tidyverse)
 
+# Measure: Claimant count
+# Source: https://www.nomisweb.co.uk/datasets/ucjsa
+# Geography: Ward, Local Authority and Combined Authority
+# Period: last 36 months
+
 read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?geography=E47000001,E08000001...E08000010,E05000650...E05000864&date=latestMINUS36-latest&gender=0&age=0&measure=2&measures=20100&select=date_name,geography_name,geography_code,gender_name,age_name,measure_name,measures_name,obs_value,obs_status_name") %>% 
   mutate(Date = as.Date(paste('01', DATE_NAME), format = '%d %B %Y'),
          Date = format(as.Date(Date), "%Y-%m"),

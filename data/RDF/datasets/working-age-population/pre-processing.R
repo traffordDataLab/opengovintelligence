4a7a1a32b82c40309342af14f6496e5a493be404
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-01-21 12:26:10 +0000

    Updated mid-year estimates for working population

--- data/RDF/datasets/working-age-population/pre-processing.R
+++ data/RDF/datasets/working-age-population/pre-processing.R
@@ -1,7 +1,7 @@
 # Measure: Mid-year population estimates for working age residents (16-64 years)
 # Source: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental
 # Geography: Electoral Ward, Local Authority District and Combined Authority
-# Period: 2016
+# Period: 2017
 
 library(tidyverse) ; library(readxl)
 
@@ -9,12 +9,12 @@ codelist <- read_csv("https://github.com/traffordDataLab/spatial_data/raw/master
   filter(area_type == "Electoral Wards") %>% 
   pull(area_code)
 
-url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental/mid2016sape19dt8/sape19dt8mid2016ward2016syoaestimatesunformatted.zip"
-download.file(url, dest = "sape19dt8mid2016ward2016syoaestimatesunformatted.zip")
-unzip("sape19dt8mid2016ward2016syoaestimatesunformatted.zip", exdir = ".")
-file.remove("sape19dt8mid2016ward2016syoaestimatesunformatted.zip")
+url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental/mid2017sape20dt8/sape20dt8mid2017ward2017syoaestimatesunformatted1.zip"
+download.file(url, dest = "sape20dt8mid2017ward2017syoaestimatesunformatted1.zip")
+unzip("sape20dt8mid2017ward2017syoaestimatesunformatted1.zip", exdir = ".")
+file.remove("sape20dt8mid2017ward2017syoaestimatesunformatted1.zip")
 
-ward <- read_excel("SAPE19DT8-mid-2016-ward-2016-syoa-estimates-unformatted.xls", sheet = 2, skip = 3) %>% 
+ward <- read_excel("SAPE20DT8-mid-2017-ward-2017-syoa-estimates-unformatted.xls", sheet = 4, skip = 3) %>% 
   filter(`Ward Code 1` %in% codelist) %>% 
   mutate(`Measure Type` = "count",
          Unit = "persons",
@@ -24,7 +24,7 @@ ward <- read_excel("SAPE19DT8-mid-2016-ward-2016-syoa-estimates-unformatted.xls"
          Unit,
          Value)
 
-la <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1879048217...1879048226&date=latestMINUS1&gender=0&c_age=203&measures=20100&select=date_name,geography_name,geography_code,obs_value") %>% 
+la <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1879048217...1879048226&date=latest&gender=0&c_age=203&measures=20100&select=date_name,geography_name,geography_code,obs_value") %>% 
   mutate(`Measure Type` = "count",
          Unit = "persons") %>% 
   select(Geography = GEOGRAPHY_CODE,
@@ -32,7 +32,7 @@ la <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geo
          Unit,
          Value = OBS_VALUE)
 
-ca <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=E47000001&date=latestMINUS1&gender=0&c_age=203&measures=20100&select=date_name,geography_name,geography_code,obs_value") %>% 
+ca <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1853882369&date=latest&gender=0&c_age=203&measures=20100&select=date_name,geography_name,geography_code,obs_value") %>% 
   mutate(`Measure Type` = "count",
          Unit = "persons") %>% 
   select(Geography = GEOGRAPHY_CODE,
